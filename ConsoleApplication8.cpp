c568f017754b48902bbbdf6e703bf1e90cf474b5
NaprasnayaSmert <Dima030305@mail.ru>
2023-11-02 15:31:18 +0300

    new_file_add

---
+++ ConsoleApplication8.cpp
@@ -0,0 +1,53 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    
+    cout << "Программа - Геометрические фигуры\n";
+    cout << "[1] Линия\n";
+    cout << "[2] Квадрат\n";
+    int x;
+    int y;
+    int a;
+    int b;
+    cout << "[+] Выберите фигуру: ";
+    cin >> x;
+    if (x == 2)
+    {
+        system("cls");
+        cout << "[+] Фигура: Квадрат\n";
+        cout << "[1] Заполненный\n";
+        cout << "[2] Пустой\n";
+        cout << "[+] Выберите тип: ";
+        cin >> y;
+        if (y == 1)
+        {
+            cout << "[+] Размер: ";
+            cin >> a;
+            cout << "[+] Текстура: ";
+            cin >> b;
+            
+            int z = 0;
+            while (z < a)
+            {
+                cout << b;
+                z++;
+            };
+            cin >> a;
+            
+            
+            //for (int i = 0; i < a; i++)
+            //{
+            //    cout << b;
+            //}
+
+        }
+
+    }
+    return 1;
+
+}
+
